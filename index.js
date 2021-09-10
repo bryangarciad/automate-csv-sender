1c76b3ae9108756e03fb6c5e22369ddee07540af
bryangarciad <brgarcia@tiempodevelopment.com>
2021-09-10 15:28:24 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+console.log( 'Starting autoamte send' );
+
+// # DEV
+const CARBON_HOST = 'https://cms.dev.carbon.motortrend.com/';
+
+// # STG
+// #CARBON_HOST=https://cms.stg.carbon.motortrend.com/
+
+// # PREPROD
+// #CARBON_HOST=https://cms.preprod.carbon.motortrend.com/
+
+// # PROD
+// #CARBON_HOST=https://cms.carbon.motortrend.com/
+
+// #LOCAL
+// const CARBON_HOST = 'http://motortrend.carbon.test/';
+
+// GO BAY GO!!!!
+(async () => {
+    // Prevent form submit
+    const form = document.querySelector('form')
+    form.addEventListener('submit', event => {
+    event.preventDefault()
+    })
+
+    // Listen for click on submit
+    const submitBtn = document.getElementById('submit')
+    submitBtn.addEventListener( 'click', async e => {
+        await run();
+    })
+ })()
+
+const run = async () => {
+    const csvInput = document.getElementById('csv')
+    let csvList = [];
+    csvList = csvInput.files;
+
+    // Loop through files
+    for (let i = 0; i < csvList.length; i++) {
+        let file = csvList.item(i);
+        await sendFile(file);
+    }
+}
+
+const sendFile = async (csvFile) => { 
+    const formData = new FormData()
+    formData.set('csv', csvFile);
+    axios.defaults.withCredentials = false;
+    try{
+        const url = CARBON_HOST + 'wp-json/mt/v1/test/pushToActiveMQCreateQueue'
+
+        console.log(url);
+        const res = await axios.post(url, formData )
+        console.log(res);
+
+    } catch (err) {
+        console.log(err);
+    }
+}
